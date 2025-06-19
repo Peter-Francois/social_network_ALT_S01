1c9f6064f97ddc4f33adfdff5b52dbbcf72be29f
Peter Francois <peter.francois237@hotmail.com>
2025-06-19 14:36:26 +0200

    feat(views): creat views for module manage_events, manage_resources and notifications

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,8 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.manage_resources, name='index'),
+    path('manage_resources/', views.manage_resources, name='manage_resources'),
+    path('manage_events/', views.manage_events, name='manage_events'),
+    path('notifications/', views.notifications, name='notifications'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'core/index.html')
 
+def manage_resources(request):
+    return render(request, 'core/manage_resources.html')
+
+def manage_events(request):
+    return render(request, 'core/manage_events.html')
+
+def notifications(request):
+    return render(request, 'core/notifications.html')
